21910080bb080ada33a163f14430862fcc7a8b3b
MOHIT SHAMBHUNATH MAURYA <msm606248@gmail.com>
2025-01-22 23:43:54 +0530

    created login and signup page UI

--- dailywage_website/urls.py
+++ dailywage_website/urls.py
@@ -1,11 +1,14 @@
 # project/urls.py
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('worker/',include('worker.urls')),
-    path('contractor/',include('contractor.urls')),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('signup/', TemplateView.as_view(template_name='signup.html'), name='signup'),
+    path('worker/', include('worker.urls')),
+    path('contractor/', include('contractor.urls')),
 ]
